72a03961a9e5758530354d2b329ebb09f7b28a54
Jordan Chapman <jchapman@ara.com>
2022-10-05 17:55:11 -0400

    Copy the colors from the in-game UI, it looks decent

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dank: {
+					900: '#160E25',
+					600: '#1c1332',
+					500: '#231c41',
+					300: '#2d223f',
+					100: '#201a4f'
+				}
+			}
+		},
 	},
 	plugins: [],
 }
