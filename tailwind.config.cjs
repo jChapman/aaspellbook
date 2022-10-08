09860c630f4f25416160411f72b58f2a828deb19
Jordan <jchapman134569@gmail.com>
2022-10-08 19:42:32 -0400

    I'm too stupid to make animation work, but I sure am trying!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				dank: {
-					900: '#160E25',
-					600: '#1c1332',
-					500: '#231c41',
-					300: '#2d223f',
-					100: '#201a4f'
-				}
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dank: {
+          999: "#0b0712",
+          900: "#160E25",
+          600: "#1c1332",
+          500: "#231c41",
+          300: "#2d223f",
+          100: "#201a4f",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        display: ["Lexend", ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        glow: "glow 1s ease-in-out infinate",
+      },
+      keyframes: {
+        glow: {
+          "0%, 100%": { transform: "rotate(-5deg)" },
+          "50%": { transform: "rotate(5deg)" },
+					//"text-shadow": "0 0 10px #ffd94d"
+        },
+      },
+    },
+  },
+  plugins: [],
+};
