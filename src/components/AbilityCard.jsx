93b4b6294fb439b7126e7749d119ff50ae33c872
Jordan <jchapman134569@gmail.com>
2022-11-07 22:57:03 -0500

    Added differences from dota to cards

--- src/components/AbilityCard.jsx
+++ src/components/AbilityCard.jsx
@@ -1,6 +1,6 @@
 import { ClockIcon } from "@heroicons/react/24/outline/index.js";
 export default function AbilityCard(props) {
-  const {ability} = props;
+  const { ability } = props;
   const values = Array.from(new Set(ability.values));
   return (
     <div className="m-2 rounded-xl bg-dank-600 shadow-lg shadow-dank-900 max-w-lg flex flex-col">
@@ -17,7 +17,7 @@ export default function AbilityCard(props) {
           ))}
         </div>
         <img
-          className="ml-auto mr-4 shadow-sm"
+          className="ml-auto mr-4 aspect-square"
           src={`https://abilityarena.com/images/ability_icons/${ability.icon}.png`}
         ></img>
       </div>
@@ -36,7 +36,7 @@ export default function AbilityCard(props) {
         <div className="flex flex-wrap items-center space-x-6">
           {ability.cooldowns.length > 0 && (
             <div className="flex items-center py-2 space-x-1">
-            <ClockIcon className="w-5 h-5"/>
+              <ClockIcon className="w-5 h-5" />
               <span>
                 {ability.cooldowns
                   .slice(0, Math.min(ability.cooldowns.length, 3))
@@ -59,15 +59,24 @@ export default function AbilityCard(props) {
         </div>
       </div>
       <div id="super" className="p-2 mt-auto mb-0 bg-dank-100">
-        <span className="text-blue-500">Super:</span>
+        <span className="text-blue-500 font-semibold">Super: </span>
         <span className="text-purple-200">
           {ability.superUpgrade.split("Super: ")[1]}
         </span>
       </div>
-      <div id="gaben" className="p-2 mb-4 bg-dank-300 ">
-        <span className="text-orange-400">Gaben:</span>
+      <div id="gaben" className="p-2 bg-dank-300 mb-3">
+        <span className="text-orange-400 font-semibold">Gaben: </span>
         <span>{ability.gabenUpgrade.split("Gaben: ")[1]}</span>
       </div>
+      {ability.differences && (
+        <div
+          id="differences"
+          className="p-2 mb-3 -mt-3 bg-slate-600 text-purple-400"
+        >
+          <span className="font-semibold">Differences from Dota: </span>
+          {ability.differences}
+        </div>
+      )}
     </div>
   );
 }
